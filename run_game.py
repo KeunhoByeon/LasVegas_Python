65fcf2a7bdb1adc6e8184f74893c7830a7194c54
변근호 <bkh5922@kookmin.kr>
2022-11-06 08:26:23 +0900

    Changed default demo

--- run_game.py
+++ run_game.py
@@ -8,7 +8,6 @@ if __name__ == '__main__':
     game_manager.add_player(slot_index=2, player_type="RuleBase")
     game_manager.add_player(slot_index=3, player_type="RuleBase")
     game_manager.add_player(slot_index=4, player_type="RuleBase")
-    game_manager.add_player(slot_index=5, player_type="RuleBase")
 
     for _ in range(GAME_NUM):
         game_manager.run()
