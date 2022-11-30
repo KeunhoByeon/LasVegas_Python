7d2de498020c5fe350ad13b5f028f7375e714a99
KeunhoByeon <bkh5922a@gmail.com>
2022-11-30 14:05:17 +0900

    Add temp codes

--- test_game.py
+++ test_game.py
@@ -3,15 +3,20 @@ import multiprocessing as mp
 import random
 
 import numpy as np
+import torch
 from joblib import Parallel, delayed
 from tqdm import tqdm
+from player.ml_player import MLPlayer
 
 from game_manager import GameManager
 
-GAME_NUM = 1000
+GAME_NUM = 5000
 RANDOM_SEED = 103
-NUM_PROCESS = mp.cpu_count()
-PLAYER_SETTING = ["RuleBase", "Random", "RuleBase", "Random"]
+NUM_PROCESS = 1
+# PLAYER_SETTING = ["MLPlayer", "RuleBase", "Random", "RuleBase", "Random"]
+# PLAYER_SETTING = ["MLPlayer", "Random", "Random", "Random", "Random"]
+PLAYER_SETTING = ["RuleBase", "RuleBase", "RuleBase", "RuleBase", "RuleBase"]
+# PLAYER_SETTING = ["RuleBase", "Random", "Random", "Random", "Random"]
 
 """
 PLAYER_SETTING = ["RuleBase", "Random", "Random"]
@@ -33,18 +38,29 @@ if __name__ == '__main__':
     if RANDOM_SEED is not None:
         random.seed(RANDOM_SEED)
         np.random.seed(RANDOM_SEED)
-    game_manager = GameManager(print_game=False)
 
-    for slot_index, player_type in enumerate(PLAYER_SETTING):
-        game_manager.add_player(slot_index=slot_index + 1, player_type=player_type)
-    rankings = Parallel(n_jobs=NUM_PROCESS)(delayed(copy.deepcopy(game_manager).run)() for _ in tqdm(range(GAME_NUM)))
+    PLAYER_SETTINGS = [
+        ["MLPlayer", "Random", "Random", "Random", "Random"],
+        ["RuleBase", "Random", "Random", "Random", "Random"]
+    ]
 
     total_ranking = [[0 for _ in range(len(PLAYER_SETTING))] for _ in range(len(PLAYER_SETTING) + 1)]
     total_ranking_sum = [0 for _ in range(len(PLAYER_SETTING) + 1)]
-    for ranking in rankings:
-        for rank, player_index in enumerate(ranking):
-            total_ranking[player_index][rank] += 1
-            total_ranking_sum[player_index] += rank
+    for PLAYER_SETTING in PLAYER_SETTINGS:
+        game_manager = GameManager(print_game=False)
+
+        for slot_index, player_type in enumerate(PLAYER_SETTING):
+            game_manager.add_player(slot_index=slot_index + 1, player_type=player_type)
+
+        if isinstance(game_manager.players_manager._player_slots[0], MLPlayer):
+            game_manager.players_manager._player_slots[0].model.load_state_dict(torch.load('./results/20221129182255/0.pth'))
+
+        rankings = Parallel(n_jobs=NUM_PROCESS)(delayed(copy.deepcopy(game_manager).run)() for _ in tqdm(range(GAME_NUM)))
+
+        for ranking in rankings:
+            for rank, player_index in enumerate(ranking):
+                total_ranking[player_index][rank] += 1
+                total_ranking_sum[player_index] += rank
 
     for i, line in enumerate(total_ranking):
         if i == 0:
